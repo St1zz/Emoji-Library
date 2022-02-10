f85361b8448dc86bd77dac511a46bcee6fa614c2
Justin Wallen <wallenju@gmail.com>
2022-02-10 08:34:10 -0500

    Initial Commit

---
+++ Emoji Library/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Emoji Library
+//
+//  Created by Justin Wallen on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
