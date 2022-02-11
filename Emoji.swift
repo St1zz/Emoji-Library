7022ead17b382abd5f3e997417abaf2097eca46d
Justin Wallen <wallenju@gmail.com>
2022-02-11 09:45:01 -0500

    yeryeyreyreyreyreyre

---
+++ Emoji.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji.swift
+//  Emoji Library
+//
+//  Created by Justin Wallen on 2/10/22.
+//
+
+import Foundation
+
+struct Emoji {
+    
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+    
+}
